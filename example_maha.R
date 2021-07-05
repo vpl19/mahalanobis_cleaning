6661a75c3a124428c5700f4b78c7a56b86339561
Bin <charles.bzhou@gmail.com>
2021-07-05 16:22:34 +0100

    Bin edits
    1. added parameters to the function, including a switch for plotting and two options for entering levels
    2. renamed to function to 'maha_clean'
    3. named the output objects
    4. added examples of use the output in example script

--- example_maha.R
+++ example_maha.R
@@ -1,4 +1,10 @@
+# Victor Lhoste
+# Example of use of maha_clean function
+# July 2021
+
+source("Mahalanobis_detection.R")
 library(MASS)  #  for simulating the data
+
 
 # Example on simulated data
 
@@ -10,10 +16,10 @@ mu1 <- 1; s1 <- 1
 mu2 <- 2; s2 <- 1.5
 
 # Parameters for bivariate normal distribution
-mu <- c(mu1,mu2) # Mean 
+mu    <- c(mu1,mu2) # Mean 
 sigma <- matrix(c(s1^2, s1*s2*rho, s1*s2*rho, s2^2),2) # Covariance
-bvn1 <- mvrnorm(N, mu = mu, Sigma = sigma ) # from MASS package
-#plot(bvn1[,1],bvn1[,2])
+bvn1  <- mvrnorm(N, mu = mu, Sigma = sigma ) # from MASS package
+# plot(bvn1[,1],bvn1[,2])
 
 # adding some outliers
 rho <- -0.8
@@ -22,14 +28,19 @@ mu2 <- 5; s2 <- 3
 mu <- c(mu1,mu2)
 sigma <- matrix(c(s1^2, s1*s2*rho, s1*s2*rho, s2^2),2)
 
-bvn1<-rbind(bvn1,mvrnorm(100, mu = mu, Sigma = sigma ))
+bvn1 <- rbind(bvn1,mvrnorm(100, mu = mu, Sigma = sigma ))
 #plot(bvn1[,1],bvn1[,2])
 
-var1<-bvn1[,1]
-var2<-bvn1[,2]
+data <- data.frame(var1 = bvn1[,1], var2 = bvn1[,2])
 
-res<-mahalanobis_plot(var1,var2) # from Mahalanonis_detection.R
-res[[1]]
-length(res[[2]])
+# from Mahalanonis_detection.R
+res <- maha_clean(data$var1, data$var2)
+# plot scatter var1 and var2 with detected outliers highlighted
+# NULL object if no outlier was detected
+res$scatter
+# row numbers of detected outliers
+length(res$outliers)
+# removing detected outliers
+if (length(res$outliers)>0) data_clean <- data[-res$outliers, ]
 
 
